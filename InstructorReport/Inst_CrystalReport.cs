071f4330911e318aa7ee925d39110c3b7656b236
AhmedEzzat1200 <ahmedayman1200@hotmail.com>
2026-05-01 20:40:41 +0300

    added instructor report

---
+++ InstructorReport/Inst_CrystalReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EduTrackPro.InstructorReport
+{
+    public partial class Inst_CrystalReport : UserControl
+    {
+        public Inst_CrystalReport()
+        {
+            InitializeComponent();
+        }
+    }
+}
